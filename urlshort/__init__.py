73552408d4bcece553c40a76940244b7a83fdb38
Shantanu Shukla <shantanu.shukla10@gmail.com>
2023-08-18 14:47:17 +0530

    complete app

---
+++ urlshort/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.secret_key = "46464674747"
+
+    from . import urlshort
+
+    app.register_blueprint(urlshort.bp)
+
+    return app
